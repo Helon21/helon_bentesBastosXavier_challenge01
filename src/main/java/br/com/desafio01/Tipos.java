84f18742076b86b3e7ac505d698a43a251110f83
Helon21 <helonbentes51@hotmail.com>
2024-04-08 21:20:45 -0300

    feat/challenge one completed
    created class Tipos and declared all primitive types

---
+++ src/main/java/br/com/desafio01/Tipos.java
@@ -0,0 +1,16 @@
+package br.com.desafio01;
+
+
+
+public class Tipos {
+
+    public byte aByte;
+    public short aShort;
+    public int aInt;
+    public long aLong;
+    public float aFloat;
+    public double aDouble;
+    public boolean aBoolean;
+    public char aChar;
+
+}
